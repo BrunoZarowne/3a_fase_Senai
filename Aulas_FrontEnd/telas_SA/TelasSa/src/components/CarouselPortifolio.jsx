72b3d05041c03cbe0fcaf536aab3b2bfd2785aa4
BRUNO WILLIAN ZAROWNE <bruno_zarowne@estudante.sesisenai.org.br>
2025-04-16 17:25:22 -0300

    arrumar problema do modal de edição

--- Aulas_FrontEnd/telas_SA/TelasSa/src/components/CarouselPortifolio.jsx
+++ Aulas_FrontEnd/telas_SA/TelasSa/src/components/CarouselPortifolio.jsx
@@ -14,29 +14,79 @@ function CarouselPortifolio() {
 
     const {imagensPort, setImagensPort} = useContext(GlobalContext)
 
-    const [img, setImg] = useState('')
-    const [fileName, setFileName] = useState('')
+    // function adcImg() {
+    //   console.log(imagensPort)
+    //   imagensPort.push({id:(imagensPort.length + 1), img:(img)})
+    // }
 
-    const handleImageChange  = (e) => {
-    const file = e.target.files[0]
-    if(file) {
-        setFileName('')
+    
+    ///Exclusão de imagens///
 
-        const reader = new FileReader()
-         reader.onloadend = () => {
-            setImg(reader.result)
-        }
-        reader.readAsDataURL(file)
+    const [imagemParaExcluir, setImagemParaExcluir] = useState(null);
 
-    }else{
-        setFileName('')
-    }}
-
-
-    function adcImg() {
-      console.log(imagensPort)
-      imagensPort.push({id:(imagensPort.length + 1), img:(img)})
+    function confirmarExclusao(event) {
+      const id = Number(event.currentTarget.getAttribute('data-iddelimglivro'));
+      setImagemParaExcluir(id); // guarda qual imagem será excluída
+      abrirModalExclusao();     // abre modal de confirmação
     }
+    
+    function excluirImg(event) {
+      if (imagemParaExcluir === null) return;
+
+        setImagensPort((prevImgs) =>
+          prevImgs.filter((img) => img.id !== imagemParaExcluir)
+      );
+
+        fecharModalExclusao();
+        setImagemParaExcluir(null); // limpa o estado
+      }
+      
+    ///Exclusão de imagens///
+      
+      
+
+    ///Adição de imagens///
+
+    const [imagemParaAdicionar, setImagemParaAdicionar] = useState(null);
+
+
+    ///Adição de imagens///
+
+
+
+
+    ///Edição de imagens///
+  
+  
+      const [imagemSelecionada, setImagemSelecionada] = useState(null);
+  
+      function editarImg(event) {
+        const idImgPort = Number(event.currentTarget.getAttribute('data-idimglivro'));
+        const imagem = imagensPort.find((img) => img.id === idImgPort);
+        setImagemSelecionada(imagem);
+        abrirModalEdit();
+      }
+  
+      function salvarEdicao() {
+        if (!imagemSelecionada) return;
+      
+        setImagensPort((prevImgs) =>
+          prevImgs.map((img) =>
+            img.id === imagemSelecionada.id ? { ...img, img: imagemSelecionada.img } : img
+          )
+        );
+      
+        fecharModalEdit();
+      }
+  
+    ///Edição de imagens///
+
+
+
+    ///Modals///
+
+    
+      ///Modal Exc/Adc/Edt///
 
     const [modalIsOpen, setIsOpen] = useState(false)
     function abrirModal() {
@@ -45,33 +95,51 @@ function CarouselPortifolio() {
     function fecharModal() {
       setIsOpen(false)
     }
-    
 
-    
-    function excluirImg(event) {
-      const idDelImgPort = Number(event.currentTarget.getAttribute('data-iddelimglivro'))
-      
-      setImagensPort((prevImgs) => prevImgs.filter((img) => img.id !== idDelImgPort));
-      
-      imagensPort.splice(idDelImgPort, idDelImgPort)
+        ///Modal Exc/Adc/Edt///
 
-      // console.log(imagensPort)
+
+        ///Modal Exc///
+
+    const [modalExcIsOpen, setModalExcOpen] = useState(false)
+    function abrirModalExclusao() {
+      setModalExcOpen(true)
+    }
+    function fecharModalExclusao(){
+      setModalExcOpen(false)
     }
 
+      ///Modal Exc///
+
+      ///Modal Edt///
     const [modalEditIsOpen, setEditIsOpen] = useState(false)
     function abrirModalEdit() {
       setEditIsOpen(true)
     }
     function fecharModalEdit() {
       setEditIsOpen(false)
-    }
-    
-    function editarImg(event) {
-      const idImgPort = event.currentTarget.getAttribute('data-idimglivro')
-      
-
+    }  
+    function editarImg() {
       abrirModalEdit()
     }
+
+      ///Modal Edt///
+
+      ///Modal Adc///
+
+    const [ModalAdcIsOpen, setModalAdcOpen] = useState(false)
+    function abrirModalAdc() {
+      setModalAdcOpen(true)
+    }
+    function fecharModalAdc() {
+      setModalAdcOpen(false)
+    }
+
+  ///Modals///
+
+
+
+
     useEffect(() => {
       console.log("Imagens atualizadas:", imagensPort);
     }, [imagensPort]);
@@ -91,7 +159,11 @@ function CarouselPortifolio() {
       }}>
         {imagensPort.map((item) =>(
           <SwiperSlide key={item.id}>
-            <img className="imgsPortifolio" src={item.img} alt="" />
+            <img
+            className="imgsPortifolio"
+            src={item.img}
+            
+            />
           </SwiperSlide>
         ))}
 
@@ -101,7 +173,7 @@ function CarouselPortifolio() {
         className="ButtonEditarImgs"
         onClick={abrirModal}
         ><label className="txtButtonEdtExc">Adicionar/Editar</label>
-        <img className="imgButtonEdtExc" src="./icon_lapis.svg"/>
+        <img className="imgButtonEdtExc" src="./icon_lapis1.svg"/>
         </button>
       </div>
       <Modal
@@ -131,18 +203,18 @@ function CarouselPortifolio() {
             <img className="imgsPortifolioModal" src={item.img} alt="" />
             <div className="buttonsEdit_Exc">
               <button 
+              className="excluirImg"
+              onClick={confirmarExclusao}
+              data-iddelimglivro={item.id}
+              >
+                <img src="./trash_icon.svg" alt="" />
+              </button>
+              <button 
               className="editarImg"
               onClick={editarImg}
               data-idimglivro={item.id}
               >
                 <img src="./edit_icon.svg" alt="" />
-              </button>
-              <button 
-              className="excluirImg"
-              onClick={excluirImg}
-              data-iddelimglivro={item.id}
-              >
-                <img src="./trash_icon.svg" alt="" />
               </button>
             </div>
           </SwiperSlide>
@@ -153,8 +225,116 @@ function CarouselPortifolio() {
 
         </div>
       </Modal>
-      
+
       <Modal
+        isOpen={modalEditIsOpen}
+        onRequestClose={fecharModalEdit}
+        ariaHideApp={false}
+        className="modalEditImg"
+      >
+        <div className="containerModalEdit">
+          <p className="txtModalEdit">Editar Imagem</p>
+
+          {imagemSelecionada && (
+            <>
+              <img
+                src={imagemSelecionada.img}
+                alt="Imagem atual"
+                className="preImagen"
+              /><br /><br />
+
+              <label
+              className="txtEditImg"
+              htmlFor="uploadInput"
+              >Selecione a imagem</label>
+
+              <input
+                id="uploadInput"
+                type="file"
+                accept="image/*"
+                style={{ display: 'none' }}
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  if (file) {
+                    const novaURL = URL.createObjectURL(file);
+                    setImagemSelecionada({
+                      ...imagemSelecionada,
+                      img: novaURL
+                    });
+                  }
+                }}
+              />
+              
+              <div className="buttonsModalEdit">
+                <button
+                onClick={salvarEdicao}
+                className="buttonSalvar"
+                >Alterar</button>
+                <button
+                onClick={fecharModalEdit}
+                className="buttonFechar"
+                >Cancelar</button>
+              </div>
+
+            </>
+          )}
+        </div>
+      </Modal>
+
+      <Modal
+      isOpen={modalExcIsOpen}
+      onRequestClose={fecharModalExclusao}
+      ariaHideApp={false}
+      className="modalExclusao"
+      >
+        <div className="containerExcImg">
+          <img src="./icon_exclusao.svg" alt="" /><br />
+          <div className="containerTxt">
+            <label className="modalExcTxt1">Tem certeza?</label><br /><br />
+            <label className="modalExcTxt2">Se confirmar não terá como recuperar o item</label><br /><br />
+          </div>
+          <div className="containerButtonsExc">
+            <button
+            onClick={excluirImg}
+            className="buttonExc"
+            >Excluir</button>
+            <button
+            onClick={fecharModalExclusao}
+            className="buttonCancelExc"
+            >Cancelar</button>
+          </div>
+        </div>
+
+      </Modal>
+
+    </div>
+  )
+}
+
+export default CarouselPortifolio
+
+
+// const [img, setImg] = useState('')
+    // const [fileName, setFileName] = useState('')
+
+    // const handleImageChange  = (e) => {
+    // const file = e.target.files[0]
+    // if(file) {
+    //     setFileName('')
+
+    //     const reader = new FileReader()
+    //      reader.onloadend = () => {
+    //         setImg(reader.result)
+    //     }
+    //     reader.readAsDataURL(file)
+
+    // }else{
+    //     setFileName('')
+    // }}
+
+
+
+/* <Modal
       className="modalEditImg"
       isOpen={modalEditIsOpen}
       onRequestClose={fecharModalEdit}
@@ -162,25 +342,25 @@ function CarouselPortifolio() {
       >
       <label className="txtModalEdit">Deseja alterar para qual imagem?</label><br />
 
-      <img className="selectImg" src={img} alt="" /><br />
+      <img className="selectImg" src="" alt="" /><br />
 
-      <label className='escolherImg' htmlFor="uploadInput">Escolher imagem</label>
-      <input id='uploadInput'
+      <label className='escolherImg' htmlFor="uploadInput">Selecione sua imagem</label>
+      <input
+      id='uploadInput'
       type='file'
       accept="image/*"
-      onChange={handleImageChange}
+      // onChange={handleImageChange}
       style={{ display: "none" }}
       />
       <div className="buttonsConf">
-        <button className="buttonAlterar">Alterar</button>
-        <button className="buttonCancelar">Cancelar</button>
+        <button
+        onClick={alterarImg}
+        className="buttonAlterar"
+        >Alterar</button>
+
+        <button
+        className="buttonCancelar"
+        onClick={fecharModalEdit}
+        >Cancelar</button>
       </div>
-      </Modal>
-
-      
-
-    </div>
-  )
-}
-
-export default CarouselPortifolio
+      </Modal> */
